a6ddc0a70542022cf2352c4f80842ca6e790fe73
ich777 <ich777@noreply.example.org>
2019-12-21 15:47:22 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export NWNX_DOTNET_SKIP=y
-CUR_V="$(find ${SERVER_DIR} -name build* | cut -d 'd' -f3)"
+export LD_PRELOAD="./NWNX_Core.so"
+export NWNX_METRICS_INFLUXDB_HOST=127.0.0.1
+export NWNX_METRICS_INFLUXDB_PORT=3306
+export NWNX_REDIS_HOST=127.0.0.1
+export NWNX_SQL_USERNAME=nwnee
+export NWNX_SQL_PASSWORD=nwnee
+export NWNX_SQL_DATABASE=nwnee
+CUR_V="$(find ${SERVER_DIR} -name build* | cut -d 'd' -f2)"
+CUR_V_BIN="$(find ${SERVER_DIR} -name binaries* | cut -d 's' -f2)"
 LAT_V="$(curl -s https://api.github.com/repos/nwnxee/unified/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'd' -f2)"
 if [ "${NWNEE_V}" == "latest" ]; then
 	NWNEE_V=$LAT_V
@@ -17,7 +25,7 @@ screen -S RedisServer -L -Logfile ${SERVER_DIR}/RedisLog.0 -d -m /usr/bin/redis-
 sleep 5
 
 if [ -z "$CUR_V" ]; then
-	echo "---NWN:EE Dedicated Server not found!---"
+	echo "---NWN:EE Dedicated Server not found, installing v${NWNEE_V}!---"
 	cd ${SERVER_DIR}
 	if wget https://nwnx.io/nwnee-dedicated-${NWNEE_V}.zip ; then
 		echo "---Sucessfully downloaded NWN:EE Dedicated Server---"
@@ -30,12 +38,30 @@ if [ -z "$CUR_V" ]; then
     rm ${SERVER_DIR}/nwnee-dedicated-${NWNEE_V}.zip
 fi
 
+if [ -z "$CUR_V_BIN" ]; then
+	echo "---NWN:EE Binaries not found, installing v${NWNEE_V}!---"
+	cd ${SERVER_DIR}/bin/linux-x86
+	if wget https://github.com/nwnxee/unified/releases/download/build${NWNEE_V}/NWNX-EE.zip ; then
+		echo "---Sucessfully downloaded NWN:EE Dedicated Server---"
+	else
+		echo "---Something went wrong, can't download NWN:EE Dedicated Server, putting server in sleep mode---"
+		sleep infinity
+	fi
+	unzip -o ${SERVER_DIR}/bin/linux-x86/NWNX-EE.zip
+    mv ${SERVER_DIR}/bin/linux-x86/Binaries/* ${SERVER_DIR}/bin/linux-x86
+    rm -R ${SERVER_DIR}/bin/linux-x86/Binaries
+	touch ${SERVER_DIR}/binaries${NWNEE_V}
+    rm ${SERVER_DIR}/bin/linux-x86/NWNX-EE.zip
+fi
+
 echo "---Prepare Server---"
 echo "---Checking if everything is in place---"
 if [ ! -d ${SERVER_DIR}/redis ]; then
 	mkdir ${SERVER_DIR}/redis
 fi
-
+if [ ! -d ${SERVER_DIR}/user ]; then
+	mkdir ${SERVER_DIR}/user
+fi
 echo "---Configuring Redis---"
 sleep 5
 echo "CONFIG SET dir ${SERVER_DIR}/redis" | redis-cli
@@ -51,5 +77,5 @@ echo "---Sleep zZz---"
 sleep infinity
 
 echo "---Start Server---"
-cd ${SERVER_DIR}
-${SERVER_DIR}/bin/linux-x86/nwnserver-linux -maxclients ${MAX_CLIENTS} -minlevel ${MINLEVEL} -maxlevel ${MAXLEVEL} -pauseandplay ${PAUSEAPLAY} -pvp ${PVP} -servervault ${SERVERVAULT} -elc ${ELC} -ilr ${ILR} -oneparty ${ONEPARTY} -difficulty ${DIFF} -autosaveinterval ${AUTO_SAV_I} -playerpassword ${PPW} -adminpassword ${APWD} -servername ${SRV_NAME} -publicserver ${PUBLIC_SRV} -reloadwhenempty ${RLD_W_E} -port ${GAME_PORT} -userdirectory ${SERVER_DIR}/save
+cd ${SERVER_DIR}/bin/linux-x86
+${SERVER_DIR}/bin/linux-x86/nwserver-linux -maxclients ${MAX_CLIENTS} -minlevel ${MINLEVEL} -maxlevel ${MAXLEVEL} -pauseandplay ${PAUSEAPLAY} -pvp ${PVP} -servervault ${SERVERVAULT} -elc ${ELC} -ilr ${ILR} -oneparty ${ONEPARTY} -difficulty ${DIFF} -autosaveinterval ${AUTO_SAV_I} -playerpassword ${PPW} -adminpassword ${APWD} -servername ${SRV_NAME} -publicserver ${PUBLIC_SRV} -reloadwhenempty ${RLD_W_E} -port ${GAME_PORT} -userdirectory ${SERVER_DIR}/user
