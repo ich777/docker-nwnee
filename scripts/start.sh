b4d8eb9ce65f1ca0b7dfaac0d37b3afd4ae648cf
ich777 <ich777@noreply.example.org>
2020-08-09 07:19:30 +0200

    added termination handler

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,4 +24,19 @@ chown -R ${UID}:${GID} /usr/bin/redis-server
 chown -R ${UID}:${GID} /usr/bin/redis-cli
 chmod -R 770 /var/lib/mysql
 chmod -R 770 /var/run/mysqld
-su ${USER} -c "/opt/scripts/start-server.sh"
+chown -R ${UID}:${GID} ${DATA_DIR}
+
+term_handler() {
+	kill -SIGTERM "$killpid"
+	wait "$killpid" -f 2>/dev/null
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+su ${USER} -c "/opt/scripts/start-server.sh" &
+killpid="$!"
+while true
+do
+	wait $killpid
+	exit 0;
+done
